7e1077b9f76e0459f7bd3d6eec54f93e6eddbb4a
Loveleileiissogood <liujiachengzhenniu@qq.com>
2016-07-19 16:00:46 +0800

    代码-Buuton前五

--- app/src/main/java/com/example/thomasliu/sudoku/MainActivity.java
+++ app/src/main/java/com/example/thomasliu/sudoku/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.thomasliu.sudoku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,120 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //第一行的Button
+        Button button11 = (Button)findViewById(R.id.hhh11);
+        Button button12 = (Button)findViewById(R.id.hhh12);
+        Button button13 = (Button)findViewById(R.id.hhh13);
+        Button button14 = (Button)findViewById(R.id.hhh14);
+        Button button15 = (Button)findViewById(R.id.hhh15);
+        Button button16 = (Button)findViewById(R.id.hhh16);
+        Button button17 = (Button)findViewById(R.id.hhh17);
+        Button button18 = (Button)findViewById(R.id.hhh18);
+        Button button19 = (Button)findViewById(R.id.hhh19);
+
+        //第二行的Button
+        Button button21 = (Button)findViewById(R.id.hhh21);
+        Button button22 = (Button)findViewById(R.id.hhh22);
+        Button button23 = (Button)findViewById(R.id.hhh23);
+        Button button24 = (Button)findViewById(R.id.hhh24);
+        Button button25 = (Button)findViewById(R.id.hhh25);
+        Button button26 = (Button)findViewById(R.id.hhh26);
+        Button button27 = (Button)findViewById(R.id.hhh27);
+        Button button28 = (Button)findViewById(R.id.hhh28);
+        Button button29 = (Button)findViewById(R.id.hhh29);
+
+        Button button31 = (Button)findViewById(R.id.hhh31);
+        Button button32 = (Button)findViewById(R.id.hhh32);
+        Button button33 = (Button)findViewById(R.id.hhh33);
+        Button button34 = (Button)findViewById(R.id.hhh34);
+        Button button35 = (Button)findViewById(R.id.hhh35);
+        Button button36 = (Button)findViewById(R.id.hhh36);
+        Button button37 = (Button)findViewById(R.id.hhh37);
+        Button button38 = (Button)findViewById(R.id.hhh38);
+        Button button39 = (Button)findViewById(R.id.hhh39);
+
+        Button button41 = (Button)findViewById(R.id.hhh41);
+        Button button42 = (Button)findViewById(R.id.hhh42);
+        Button button43 = (Button)findViewById(R.id.hhh43);
+        Button button44 = (Button)findViewById(R.id.hhh44);
+        Button button45 = (Button)findViewById(R.id.hhh45);
+        Button button46 = (Button)findViewById(R.id.hhh46);
+        Button button47 = (Button)findViewById(R.id.hhh47);
+        Button button48 = (Button)findViewById(R.id.hhh48);
+        Button button49 = (Button)findViewById(R.id.hhh49);
+
+        Button button51 = (Button)findViewById(R.id.hhh51);
+        Button button52 = (Button)findViewById(R.id.hhh52);
+        Button button53 = (Button)findViewById(R.id.hhh53);
+        Button button54 = (Button)findViewById(R.id.hhh54);
+        Button button55 = (Button)findViewById(R.id.hhh55);
+        Button button56 = (Button)findViewById(R.id.hhh56);
+        Button button57 = (Button)findViewById(R.id.hhh57);
+        Button button58 = (Button)findViewById(R.id.hhh58);
+        Button button59 = (Button)findViewById(R.id.hhh59);
+
+        Button button11 = (Button)findViewById(R.id.hhh11);
+        Button button12 = (Button)findViewById(R.id.hhh12);
+        Button button13 = (Button)findViewById(R.id.hhh13);
+        Button button14 = (Button)findViewById(R.id.hhh14);
+        Button button15 = (Button)findViewById(R.id.hhh15);
+        Button button16 = (Button)findViewById(R.id.hhh16);
+        Button button17 = (Button)findViewById(R.id.hhh17);
+        Button button18 = (Button)findViewById(R.id.hhh18);
+        Button button19 = (Button)findViewById(R.id.hhh19);
+
+        Button button11 = (Button)findViewById(R.id.hhh11);
+        Button button12 = (Button)findViewById(R.id.hhh12);
+        Button button13 = (Button)findViewById(R.id.hhh13);
+        Button button14 = (Button)findViewById(R.id.hhh14);
+        Button button15 = (Button)findViewById(R.id.hhh15);
+        Button button16 = (Button)findViewById(R.id.hhh16);
+        Button button17 = (Button)findViewById(R.id.hhh17);
+        Button button18 = (Button)findViewById(R.id.hhh18);
+        Button button19 = (Button)findViewById(R.id.hhh19);
+
+        Button button11 = (Button)findViewById(R.id.hhh11);
+        Button button12 = (Button)findViewById(R.id.hhh12);
+        Button button13 = (Button)findViewById(R.id.hhh13);
+        Button button14 = (Button)findViewById(R.id.hhh14);
+        Button button15 = (Button)findViewById(R.id.hhh15);
+        Button button16 = (Button)findViewById(R.id.hhh16);
+        Button button17 = (Button)findViewById(R.id.hhh17);
+        Button button18 = (Button)findViewById(R.id.hhh18);
+        Button button19 = (Button)findViewById(R.id.hhh19);
+
+        Button button11 = (Button)findViewById(R.id.hhh11);
+        Button button12 = (Button)findViewById(R.id.hhh12);
+        Button button13 = (Button)findViewById(R.id.hhh13);
+        Button button14 = (Button)findViewById(R.id.hhh14);
+        Button button15 = (Button)findViewById(R.id.hhh15);
+        Button button16 = (Button)findViewById(R.id.hhh16);
+        Button button17 = (Button)findViewById(R.id.hhh17);
+        Button button18 = (Button)findViewById(R.id.hhh18);
+        Button button19 = (Button)findViewById(R.id.hhh19);
+
+        Button button11 = (Button)findViewById(R.id.hhh11);
+        Button button12 = (Button)findViewById(R.id.hhh12);
+        Button button13 = (Button)findViewById(R.id.hhh13);
+        Button button14 = (Button)findViewById(R.id.hhh14);
+        Button button15 = (Button)findViewById(R.id.hhh15);
+        Button button16 = (Button)findViewById(R.id.hhh16);
+        Button button17 = (Button)findViewById(R.id.hhh17);
+        Button button18 = (Button)findViewById(R.id.hhh18);
+        Button button19 = (Button)findViewById(R.id.hhh19);
+
+        Button button11 = (Button)findViewById(R.id.hhh11);
+        Button button12 = (Button)findViewById(R.id.hhh12);
+        Button button13 = (Button)findViewById(R.id.hhh13);
+        Button button14 = (Button)findViewById(R.id.hhh14);
+        Button button15 = (Button)findViewById(R.id.hhh15);
+        Button button16 = (Button)findViewById(R.id.hhh16);
+        Button button17 = (Button)findViewById(R.id.hhh17);
+        Button button18 = (Button)findViewById(R.id.hhh18);
+        Button button19 = (Button)findViewById(R.id.hhh19);
+
+
     }
+
 }
